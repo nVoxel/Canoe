d6f9f0d51ef19ba7bf8fa04fe408f625627847e9
nVoxel <nvoxel@outlook.com>
2024-01-16 02:29:48 +0300

    Display navigation rail in landscape orientation, animations depend on current orientation

--- source/compose-ui/src/main/java/com/voxeldev/canoe/compose/ui/root/RootContent.kt
+++ source/compose-ui/src/main/java/com/voxeldev/canoe/compose/ui/root/RootContent.kt
@@ -1,6 +1,9 @@
 package com.voxeldev.canoe.compose.ui.root
 
+import android.content.res.Configuration
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
@@ -9,6 +12,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
@@ -16,10 +20,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.arkivanov.decompose.extensions.compose.jetpack.stack.Children
 import com.arkivanov.decompose.extensions.compose.jetpack.subscribeAsState
+import com.voxeldev.canoe.compose.ui.components.CustomNavigationRail
 import com.voxeldev.canoe.compose.ui.components.decompose.slideTabAnimation
 import com.voxeldev.canoe.compose.ui.dashboard.DashboardContent
 import com.voxeldev.canoe.compose.ui.leaderboards.LeaderboardsContent
@@ -49,27 +56,52 @@ fun RootContent(component: RootComponent) {
             )
         )
 
-        Scaffold(
-            bottomBar = {
-                BottomNavigation(component = component)
-            },
-        ) { paddingValues ->
-            Children(
-                component = component,
-                modifier = Modifier.padding(
-                    bottom = paddingValues.calculateBottomPadding(),
-                )
-            )
+        LocalConfiguration.current.orientation.let { orientation ->
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                VerticalLayout(component = component)
+            } else {
+                HorizontalLayout(component = component)
+            }
         }
     }
 }
 
 @Composable
-private fun Children(component: RootComponent, modifier: Modifier = Modifier) {
+private fun VerticalLayout(component: RootComponent) {
+    Scaffold(
+        bottomBar = {
+            BottomNavigation(component = component)
+        },
+    ) { paddingValues ->
+        Children(
+            component = component,
+            modifier = Modifier.padding(
+                bottom = paddingValues.calculateBottomPadding(),
+            ),
+            isHorizontalLayout = false,
+        )
+    }
+}
+
+@Composable
+private fun HorizontalLayout(component: RootComponent) {
+    Row {
+        SideNavigation(component = component)
+        Children(
+            component = component,
+            isHorizontalLayout = true,
+        )
+    }
+}
+
+@Composable
+private fun Children(component: RootComponent, modifier: Modifier = Modifier, isHorizontalLayout: Boolean) {
     Children(
         stack = component.childStack,
         modifier = modifier,
-        animation = slideTabAnimation { index() },
+        animation = slideTabAnimation(
+            orientation = if (isHorizontalLayout) Orientation.Vertical else Orientation.Horizontal
+        ) { index() },
     ) {
         when (val child = it.instance) {
             is Root.Child.DashboardChild -> DashboardContent(component = child.component)
@@ -135,6 +167,77 @@ private fun BottomNavigation(component: RootComponent, modifier: Modifier = Modi
         )
 
         NavigationBarItem(
+            selected = activeComponent is Root.Child.SettingsChild,
+            onClick = component::onSettingsTabClicked,
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = "Settings",
+                )
+            },
+            label = {
+                Text(text = "Settings")
+            },
+            alwaysShowLabel = false,
+        )
+    }
+}
+
+@Composable
+private fun SideNavigation(component: RootComponent, modifier: Modifier = Modifier) {
+    val childStack by component.childStack.subscribeAsState()
+    val activeComponent = childStack.active.instance
+
+    CustomNavigationRail(
+        modifier = modifier
+            .zIndex(1f),
+    ) {
+        NavigationRailItem(
+            selected = activeComponent is Root.Child.DashboardChild,
+            onClick = component::onDashboardTabClicked,
+            icon = {
+                Icon(
+                    imageVector = AdditionalIcons.Dashboard,
+                    contentDescription = "Dashboard",
+                )
+            },
+            label = {
+                Text(text = "Dashboard")
+            },
+            alwaysShowLabel = false,
+        )
+
+        NavigationRailItem(
+            selected = activeComponent is Root.Child.LeaderboardsChild,
+            onClick = component::onLeaderboardsTabClicked,
+            icon = {
+                Icon(
+                    imageVector = AdditionalIcons.Leaderboard,
+                    contentDescription = "Leaderboards",
+                )
+            },
+            label = {
+                Text(text = "Leaderboards")
+            },
+            alwaysShowLabel = false,
+        )
+
+        NavigationRailItem(
+            selected = activeComponent is Root.Child.ProjectsChild,
+            onClick = component::onProjectsTabClicked,
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.List,
+                    contentDescription = "Projects",
+                )
+            },
+            label = {
+                Text(text = "Projects")
+            },
+            alwaysShowLabel = false,
+        )
+
+        NavigationRailItem(
             selected = activeComponent is Root.Child.SettingsChild,
             onClick = component::onSettingsTabClicked,
             icon = {
